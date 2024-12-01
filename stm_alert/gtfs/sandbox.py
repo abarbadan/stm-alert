670ef7d942f8e303b30f020b76aa05d09a343a4f
Daniel Abarbanel <abarbadan@gmail.com>
2024-12-01 01:12:07 +0000

    updated error message

--- stm_alert/gtfs/sandbox.py
+++ stm_alert/gtfs/sandbox.py
@@ -1,5 +1,6 @@
 from google.transit import gtfs_realtime_pb2
 from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 from stm_alert.gtfs.constants import VEHICLE_POSITION_API_ENDPOINT
 
 api_key = "l7f6c826a7eeb34fb8ba511c679b121c37"
@@ -7,7 +8,12 @@ api_key = "l7f6c826a7eeb34fb8ba511c679b121c37"
 feed = gtfs_realtime_pb2.FeedMessage()
 request = Request(VEHICLE_POSITION_API_ENDPOINT)
 request.add_header("apiKey", api_key)
-response = urlopen(request)
+try:
+    response = urlopen(request)
+except HTTPError as e:
+    print(f"HTTP error: {e.code}")
+    print(f"Error message: {e.read().decode('utf-8')}")
+    exit(1)
 
 print(f"Status: {response.status_code}")
 
